ed80a6ff67fbc7a12d9454098a8c852581d6234b
dks <june3474@gmail.com>
2021-11-12 10:17:59 +0900

    Add darcula and vscode dark+ themes

---
+++ elpa/jetbrains-darcula-theme-20210602.1430/jetbrains-darcula-theme-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from jetbrains-darcula-theme.el  -*- no-byte-compile: t -*-
+(define-package "jetbrains-darcula-theme" "20210602.1430" "A complete port of the default JetBrains Darcula theme" 'nil :commit "f57c359044ff1fa90db62a60b6691ff8d65c82f3" :authors '(("Ian Y.E. Pan")) :maintainer '("Ian Y.E. Pan") :url "https://github.com/ianpan870102/jetbrains-darcula-emacs-theme")
