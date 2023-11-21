489a8ff04b34e6176ae56c9a30f7c0e9447d0372
dks <june3474@gmail.com>
2023-11-22 00:05:59 +0900

    Add Windows-specific option to early-init.el

--- early-init.el
+++ early-init.el
@@ -1,9 +1,16 @@
-(setq default-frame-alist
-      '((width . 90)
-        (height . 40)
-        (font . "Monospace-14")
-        (background-mode . dark)
-        (background-color . "gray10")
-        (foreground-color . "white")
-        (mouse-color . "white")
-        (vertical-scroll-bars . right)))
+(let (my-font my-height)
+  (if (eq system-type 'windows-nt)
+      (setq my-font "D2Coding-12"
+            my-height 38)
+    (setq my-font "Monospace-14"
+          my-height 40))
+  
+  (setq default-frame-alist
+        `((width . 90)
+          (height . ,my-height)
+          (font . ,my-font)
+          (background-mode . dark)
+          (background-color . "gray10")
+          (foreground-color . "white")
+          (mouse-color . "white")
+          (vertical-scroll-bars . right))))
