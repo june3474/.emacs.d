13e62ddc5a8ab575da0c260e32bb1e388d27d9dc
dks <june3474@gmail.com>
2024-09-07 13:27:09 +0900

    Add ws-butler package files

---
+++ elpa/ws-butler-20201117.1528/ws-butler-pkg.el
@@ -0,0 +1,4 @@
+(define-package "ws-butler" "20201117.1528" "Unobtrusively remove trailing whitespace." 'nil :commit "e3a38d93e01014cd47bf5af4924459bd145fd7c4" :url "https://github.com/lewang/ws-butler")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
